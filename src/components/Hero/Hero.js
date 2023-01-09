81842dddbfdf15e49023aeeefe3e11b80cf1d9d9
ram pandey <63231427+ram-kp@users.noreply.github.com>
2023-01-09 19:31:22 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
       </SectionTitle>
       <Hr />
       <SectionText>
-        Hi There, I am <p style={{fontWeight: "bolder", color:"white"}}> Ram Pandey | 3rd year Computer Science Student | B.E. from BIT Bangalore </p> An aspiring Full stack developer with aptness in Cloud Infrastructures.
+        Hi There, I am <p style={{fontWeight: "bolder", color:"white"}}> Ram Pandey | 4th year Computer Science Student | B.E. from BIT Bangalore </p> An aspiring Full stack developer with aptness in Cloud Infrastructures.
         <br />
         Always ready and flexible to learn new technologies, and adaptable to work environments.          
       </SectionText>
